e6e3645a8b6f4610bd56844e43b2b741cfcffe8e
xXRex45Xx <esrom.mulugeta@gmail.com>
2022-02-20 11:36:29 +0300

    Implmented Nav Bar as Header Component

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import {
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage  from './pages/shoppage/shoppage.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Routes>
         <Route path='/'>
           <Route index element={<HomePage />}/>
